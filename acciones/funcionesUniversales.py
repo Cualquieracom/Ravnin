56d3470af6daf778ee3f19b9f56da0241306f27f
Kass <87446950+Cualquieracom@users.noreply.github.com>
2022-01-01 18:39:53 +0100

    Update funcionesUniversales.py

--- acciones/funcionesUniversales.py
+++ acciones/funcionesUniversales.py
@@ -22,8 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 try:
-  import aminofix
-  amino = aminofix
+  import amino
+  amino = amino
 except:
   import amino
 
